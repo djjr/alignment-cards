1fa6ed3357c170414f075cf90b4826222157fe59
Dan Ryan <djjrjr@gmail.com>
2025-10-16 14:04:07 -0400

    Add new alignment principles to the cards array

--- alignmentcards-v0.js
+++ alignmentcards-v0.js
@@ -6,7 +6,18 @@ export const categories = [
     "pathology": "normative void", 
     "color": "#E6FFE9",
     "description": "Alignment principles are contestable, general-purpose, broadly recognized ethical or social or normative commitments that can serve as warrants for recommending or evaluating an agent's course of action in contexts where alignment and cooperation with others matters."
+  },
+  {
+    "code": "AB",
+    "name": "Rewarding A while hoping for B",
+    "description": "TBS"
+  },
+  {
+    "code": "TEST",
+    "name": "TEST",
+    "description": "TEST"
   }
+
 ];
 
 
