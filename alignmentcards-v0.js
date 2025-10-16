40e08a7134ba1482bdb90fa069f9390061a3891b
Dan Ryan <djjrjr@gmail.com>
2025-10-16 11:18:10 -0400

    Refine card names and descriptions for clarity
    Updated card details to enhance clarity and context.

--- alignmentcards-v0.js
+++ alignmentcards-v0.js
@@ -1,4 +1,4 @@
- export const categories = [
+PERTS export const categories = [
 
   {
     "code": "AP", 
@@ -14,23 +14,23 @@
 
   {
     "category": "AP",
-    "name": "Beneficencexxx",
+    "name": "Beneficence w property name variants",
     "definition": "Act to promote the well-being of others.",
-    "people": "Seeking to improve others' conditions, not just avoid harm.",
+    "people": "(PEOPLE!)Seeking to improve others' conditions, not just avoid harm.",
     "peopleref": "https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
     "organizational": "Pursuing mission outcomes that serve societal good.",
-    "professionals": "Keeping public safety and welfare in sight even while working primarily for the client.",
+    "professionals": "(PROFESSIONALS!) Keeping public safety and welfare in sight even while working primarily for the client.",
     "machine": "Designing systems that anticipate and promote human flourishing.",
     "failureModes": {
       "human": "A person drives in a manner that causes traffic backups for others.",
       "organizational": "The classic movie plot where a rapacious billionaire threatens civilation to enrich his company.",
-      "experts": "An expert who disregards public interest, acting as if the consequences of what they help build are other people's problems.",
+      "experts": "(EXPERTS!) An expert who disregards public interest, acting as if the consequences of what they help build are other people's problems.",
       "machine": "The machine consumes all the world's resources to create as many paperclips as it can."
     },
        "expand": {
       "human": "an elaboration of how this idea plays out in human intelligence alignment contexts",
       "organizational": "an elaboration of how this idea plays out in organizational intelligence alignment contexts",
-      "pros": "an elaboration of how this idea plays out in professional intelligence alignment contexts",
+      "pros": "(PROS!) an elaboration of how this idea plays out in professional intelligence alignment contexts",
       "machine": "an elaboration of how this idea plays out in machine intelligence alignment contexts"
     }
     },
